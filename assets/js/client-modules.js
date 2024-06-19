41c4650e36faabfde44d23d5d71672589d6c9b15
josecantero <josedejesuscantero@gmail.com>
2024-06-19 05:00:40 -0400

    ksdkflkdsfjlksd

---
+++ assets/js/client-modules.js
@@ -0,0 +1,138 @@
+var require = (function() {
+  // function join copied from creationix/path.js
+  function join(/* path segments */) {
+    var parts = [];
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      parts = parts.concat(arguments[i].split("/"));
+    }
+    var newParts = [];
+    for (i = 0, l = parts.length; i < l; i++) {
+      var part = parts[i];
+      if (!part || part === ".") continue;
+      if (part === "..") newParts.pop();
+      else newParts.push(part);
+    }
+    if (parts[0] === "") newParts.unshift("");
+    return newParts.join("/") || (newParts.length ? "/" : ".");
+  }
+
+  // helpers
+  var with_js = name => name + (name.substr(-3) == '.js' ? '' : '.js'),
+      without_js = name => name.substr(-3) == '.js'
+                         ? name.substr(0, name.length - 3)
+                         : name,
+      without_basedir = url => {
+        if (is_beggining_with(url, main.dirname)) {
+          return url.substr(main.dirname.length + 1);
+        }
+      },
+      is_beggining_with = (full, start) => (full.substr(0, start.length) == start),
+      to_arr = (o) => Object.keys(o).map(k => o[k] || k ),
+      to_obj = (ks, vs) => ks.reduce((o,k,i)=> {o[k] = vs[i]; return o;}, {});
+
+  // Module
+  class Module {
+    constructor(filename) {
+      this.loaded = false;
+
+      this.filename = filename;
+      this.dirname = join(filename, filename.substr(-1) == '/' ? '.' : '..');
+
+      this.children = [];
+      this.prepare = imports => null,
+      this.exports = {};
+    }
+
+    addChildren(module) {
+      this.children.push(module);
+      module.parent = this;
+    }
+
+    require(requirement) {
+      var that = this;
+      if (Array.isArray(requirement)) {
+        return Promise.all(requirement.map(this.require.bind(this)));
+      } else if (typeof requirement === 'object') {
+        return new Promise(function(resolve, reject) {
+          var keys = Object.keys(requirement);
+          that.require(to_arr(requirement))
+            .then(imports => resolve(to_obj(keys, imports)))
+            .catch(reject);
+        });
+      } else if (typeof requirement === 'string') {
+        var url = that.resolve(with_js(requirement));
+        var identifier = without_js(without_basedir(url));
+        var module = modules[identifier];
+
+        return new Promise(function(resolve, reject){
+          if (module) {
+            if(module.loaded) {
+              resolve(module.exports);
+            } else {
+              $(module).on('loaded', (e) => resolve(module.exports));
+            }
+          } else {
+            module = modules[identifier] = new Module(url);
+            that.addChildren(module);
+            // fetching module definition
+            $.ajax(url, ajaxOpts)
+            .done(response => {
+              // execute module definition
+              new Function('exports', 'require', 'module', '__filename', '__dirname', response)
+              (module.exports, publicProps(module.require.bind(module)), module, module.filename, module.dirname);
+              // getting module requirement
+              module.require(module.requirement)
+              .then(imports => {
+                // prepare after requirement resolved
+                module.prepare(imports);
+                delete module.prepare;
+                // finalize and resolve
+                module.loaded = true;
+                $(module).trigger('loaded');
+                resolve(module.exports);
+              })
+              .catch(reject);
+            })
+            .fail(reject);
+          }
+        });
+      } else {
+        return new Promise((resolve, reject) => {resolve(requirement)});
+      }
+    }
+
+    resolve(name) {
+      var base = main.dirname;
+      if (name.substr(0, 1) === '/') {
+        base = '/';
+      } else if (name.substr(0, 2) === './' || name.substr(0, 3) === '../') {
+        base = this.dirname;
+      }
+      return join(base, name);
+    }
+  }
+
+  // core
+  var main = new Module(window.location.pathname),
+    ajaxOpts = { cache: false, dataType: 'text' },
+    modules = {};
+
+  function publicProps(fn) {
+    fn.modules = modules;
+    fn.main = main;
+    fn.basedir = newBasedir => {
+      if (typeof newBasedir == 'string') {
+        main.dirname = main.resolve(newBasedir);
+        main.children = [];
+        fn.modules = modules = {};
+      };
+      return main.dirname;
+    };
+    return fn;
+  }
+
+  // require function
+  return publicProps(main.require.bind(main));
+}({
+
+}));
---
+++ assets/js/coinpaprika.js
@@ -0,0 +1,167 @@
+( async () => {
+    if ( typeof module !== 'undefined' && module.exports ) {
+        console.log("entro en 1");
+        var axios = require( 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js' ), exports = module.exports;
+    } else {
+        console.log("entro en 2");
+        var exports = window.cp = {};
+    }
+    const instance = axios.create( {
+        baseURL: 'https://api.coinpaprika.com/v1',
+        headers: typeof window !== 'undefined' ? {} : {
+            'User-Agent': 'coinpaprika-js'
+        },
+        timeout: 30000
+    } );
+    async function request( url, params = {} ) {
+        return new Promise( ( resolve, reject ) => {
+            instance.get( url, { params } )
+                .then( function ( response ) {
+                    resolve( response.data );
+                } )
+                .catch( function ( error ) {
+                    if ( error.response ) console.warn( error.response.data );
+                    throw error.message;
+                } );
+        } );
+    }
+
+    // Get market overview data
+    exports.global = async ( params = {} ) => {
+        return request( '/global', params );
+    };
+
+    // List coins
+    exports.coins = async ( params = {} ) => {
+        return request( '/coins', params );
+    };
+
+    // Get coin by ID
+    exports.coin = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}`, params );
+    };
+
+    // Get markets by coin ID (USD,BTC,ETH,PLN)
+    exports.markets = async ( id = "btc-bitcoin", params = { quotes: "USD" } ) => {
+        return request( `/coins/${id}/markets`, params );
+    };
+
+    // Get exchanges by coin ID
+    exports.exchanges = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/exchanges`, params );
+    };
+
+    // Get events by coin ID
+    exports.events = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/events`, params );
+    };
+
+    // Get tweets by coin ID
+    exports.twitter = async ( id = "btc-bitcoin", params = {} ) => {
+        return request( `/coins/${id}/twitter`, params );
+    };
+
+    // Get tickers for all coins (USD,BTC,ETH)
+    exports.tickers = async ( params = { quotes: "USD" } ) => {
+        return request( '/tickers', params );
+    };
+
+    // Get ticker information for a specific coin (USD,BTC,ETH)
+    exports.ticker = async ( id, params = {} ) => {
+        return request( `/ticker/${id}`, params );
+    };
+
+    // Get historical ticker information for a specific coin (USD,BTC,ETH)
+    exports.historical = async ( id, start, params = { end: false, interval: "5m", quote: "USD", limit: 1000 } ) => {
+        params.start = start;
+        if ( typeof params.end == "undefined" || !params.end ) params.end = new Date().toISOString();
+        return request( `/tickers/${id}/historical`, params );
+    };
+
+    // Convert 5k USDT BTC = 0.66 BTC
+    exports.convert = async ( amount, baseId, quoteId ) => {
+        return request( `/price-converter`, {base_currency_id:baseId, quote_currency_id:quoteId, amount} );
+    };
+
+    // List exchanges (USD,BTC,ETH,PLN)
+    exports.exchangeList = async ( params = { quotes: "USD" } ) => {
+        return request( "/exchanges", params );
+    };
+
+    // Get exchange by ID (USD,BTC)
+    exports.exchange = async ( id, params = { quotes: "USD" } ) => {
+        return request( `/exchanges/${id}`, params );
+    };
+
+    // Get markets by exchange ID (USD,BTC,ETH,PLN)
+    exports.exchangeMarkets = async ( id, params = { quotes: "USD" } ) => {
+        return request( `/exchanges/${id}/markets`, params );
+    };
+
+    // Search
+    exports.search = async ( query = "btc", params = { limit: 6, c: "currencies,exchanges,icos,people,tags" } ) => {
+        params.q = query;
+        return request( `/search`, params );
+    };
+
+    // Get 24h OHLC candle (USD,BTC)
+    exports.candle = async ( id = "btc-bitcoin", params = { quote: "USD" } ) => {
+        return request( `/coins/${id}/ohlcv/latest/`, params );
+    };
+    
+    // Get historical OHLCV information for a specific coin (USD,BTC)
+    exports.candles = async ( id, start = false, params = { end: false, quote: "USD", limit: 366 } ) => {
+        if ( !start ) {
+            let year_ago = new Date();
+            year_ago.setFullYear( year_ago.getFullYear() - 1 );
+            start = year_ago;
+        }
+        if ( typeof start == "string" ) start = new Date( start );
+        params.start = start.toISOString();
+        if ( typeof params.end == "undefined" || !params.end ) params.end = new Date().toISOString();
+        return request( `/coins/${id}/ohlcv/historical`, params );
+    };
+
+    // Get Today's OHLC candle: rolls over at 23:59:59 (USD,BTC)
+    exports.today = async ( id = "btc-bitcoin", params = { quote: "USD" } ) => {
+        return request( `/coins/${id}/ohlcv/today/`, params );
+    };
+
+    // List tags
+    exports.tags = async ( fields = "coins,icos", params = {} ) => {
+        params.additional_fields = fields;
+        return request( "/tags", params );
+    };
+
+    // Get tag by ID
+    exports.tag = async ( id = "blockchain-service", fields = "coins,icos", params = {} ) => {
+        params.additional_fields = fields;
+        return request( `/tags/${id}`, params );
+    };
+
+    // Get people by ID
+    exports.people = async ( id = "vitalik-buterin", params = {} ) => {
+        return request( `/people/${id}`, params );
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // UNSUPPORTED API ENDPOINTS:
+    // These are not documented on https://api.coinpaprika.com and could stop working at any time
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Get market depth by ID
+    exports.depth = async ( id = "btc-bitcoin", currency = "usd-us-dollars", params = {} ) => {
+        return request( `https://coinpaprika.com/ajax/transparency/markets-depth/${id}/${currency}/`, params );
+    };
+
+    // Get market capitalization (24h 7d 30d 1q 1y YTD MAX)
+    exports.mcap = async ( timeframe = "24h", params = {} ) => {
+        return request( `https://coinpaprika.com/overview/data/total/${timeframe}/`, params );
+    };
+
+    // Get market capitalization excluding BTC (24h 7d 30d 1q 1y YTD MAX)
+    exports.altcap = async ( timeframe = "24h", params = {} ) => {
+        return request( `https://coinpaprika.com/overview/data/totalExclude/${timeframe}/`, params );
+    };
+
+} )();
